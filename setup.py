9e7a4c382b4da98829743f8ebf899835a464fd05
yixiangchen1995 <yixiangchen1995@gmail.com>
2026-01-19 11:26:20 +0800

    fix package

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ Author: Yixiang Chen
 version: 
 Date: 2025-03-24 15:57:41
 LastEditors: Yixiang Chen
-LastEditTime: 2026-01-19 10:58:03
+LastEditTime: 2026-01-19 11:25:16
 '''
 
 from setuptools import setup, find_packages
 
-VERSION = '0.4.1' 
+VERSION = '0.5.1' 
 DESCRIPTION = 'Text Front for TTS'
 #LONG_DESCRIPTION = 'Data Package for TTS with a slightly longer description'
 LONG_DESCRIPTION = open("README.md", encoding="utf-8").read()
@@ -35,7 +35,6 @@ setup(
             'jieba',
             'jieba_fast',
             'pypinyin',
-            'WeTextProcessing==1.0.3',
             'langid'
         ], # add any additional packages that ## add tinytag package
         
